dfdf1e5a6da3d2d4ebee578ce846902da6294c5e
contactsriharis <contactsriharis@gmail.com>
2022-01-07 09:23:56 +0530

    Initial Commit

---
+++ PPS(sem-1)_Miniporject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PPS(sem-1)_Miniporject
+//
+//  Created by Srihari Sridhar on 07/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
