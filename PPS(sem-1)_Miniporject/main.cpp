2725f340d9310ef3dfeb80fdd98ed0ed42769e01
contactsriharis <contactsriharis@gmail.com>
2022-01-07 09:42:36 +0530

    Initial Setup

--- PPS(sem-1)_Miniporject/main.cpp
+++ PPS(sem-1)_Miniporject/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  PPS(sem-1)_Miniporject
-//
-//  Created by Srihari Sridhar on 07/01/22.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
